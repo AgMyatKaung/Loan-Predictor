e2837047bda05912e4f01fa7a032d701f87e525a
Aung Myat Kaung <107551174+AgMyatKaung@users.noreply.github.com>
2023-08-23 21:28:27 +0630

    Update loan.py

--- loan.py
+++ loan.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.linear_model import LinearRegression
 import joblib
 
-model = joblib.load("D:\Programming\AI\loan\loan_trainModel.pkl")
+model = joblib.load("loan_trainModel.pkl")
 
 def predict_bank_balance(salary):
     predicted_balance = model.predict([[salary]])
